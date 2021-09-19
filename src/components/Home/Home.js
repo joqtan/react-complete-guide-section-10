7607ab6ffbabfd1b510a9b3ef614707f270bec8c
Joqtan Marmol <joqtanmarmol@gmail.com>
2021-09-18 21:27:30 -0600

    section 10 completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import AuthContext from '../../context/auth-context'
+import Button from '../UI/Button/Button'
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from '../UI/Card/Card'
+import classes from './Home.module.css'
 
-const Home = (props) => {
+const Home = () => {
+  const { onLogout } = useContext(AuthContext)
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Logout</Button>
     </Card>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
